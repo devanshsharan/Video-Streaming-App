078c06676d3d38cafaf25c9fbfa9fe14a9ec10c5
devansh1356 <devanshsharan.gupta@beehyv.com>
2024-11-04 11:50:18 +0530

    env removed

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ io.on("connection", (socket) => {
   })
 
   });
-  const PORT = process.env.PORT || 8000;
+  const PORT = 8000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
